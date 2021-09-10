15aa48d0ea5eed39fa0ef227b554dccf4df4dc57
João Marcos Soares <jomamts@gmail.com>
2021-09-09 21:10:34 -0300

    feat: configs adiconais

--- src/main/java/dev/joao/desafioUnimed/controllers/ClienteController.java
+++ src/main/java/dev/joao/desafioUnimed/controllers/ClienteController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -30,6 +31,7 @@ import dev.joao.desafioUnimed.exceptions.MethodArgumentTypeMismatchResponse;
 import dev.joao.desafioUnimed.services.ClienteService;
 import lombok.AllArgsConstructor;
 
+@CrossOrigin
 @RestController
 @RequestMapping("api/v1/clientes")
 @AllArgsConstructor(onConstructor = @__(@Autowired))
